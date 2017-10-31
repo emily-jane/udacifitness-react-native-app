2f99676dd68f4d9ef811284acc8f148e7499aa68
Emily Carson <Macbookpro@Macbooks-MacBook-Pro.local>
2017-10-31 19:21:31 +0000

    added udacity calendar

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
-import AddEntry from './components/AddEntry';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <View style={{height: 20}} />
+          <History />
         </View>
       </Provider>
-    );
+    )
   }
 }
