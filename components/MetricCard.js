5bcd72b9cd8a732104e4f146b8ef0bb93be9e754
Emily Carson <Macbookpro@Macbooks-MacBook-Pro.local>
2017-11-01 20:52:14 +0000

    added tab navigator, not currently working, expo crashing

--- components/MetricCard.js
+++ components/MetricCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import DateHeader from './DateHeader';
-import { getMetricDataInfo } from '../utils/helpers';
+import { getMetricMetaInfo } from '../utils/helpers';
 import { gray } from '../utils/colors';
 
 export default function MetricCard ({ date, metrics }) {
@@ -9,7 +9,7 @@ export default function MetricCard ({ date, metrics }) {
     <View>
       {date && <DateHeader date={date} />}
       {Object.keys(metrics).map((metric) => {
-        const { getIcon, displayName, unit, backgroundColor } = getMetricDataInfo(metric);
+        const { getIcon, displayName, unit, backgroundColor } = getMetricMetaInfo(metric);
 
         return (
           <View style={styles.metric} key={metric}>
